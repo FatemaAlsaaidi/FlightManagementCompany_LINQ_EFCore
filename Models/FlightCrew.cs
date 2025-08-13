a9e4b150f7f9ba19a34cc773ef478f2e38d7c26e
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-13 10:20:43 +0400

    create FlightCrew model with one property

---
+++ Models/FlightCrew.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany_LINQ_EFCore.Models
+{
+    public class FlightCrew
+    {
+        public string RoleOnFlight;
+    }
+}
