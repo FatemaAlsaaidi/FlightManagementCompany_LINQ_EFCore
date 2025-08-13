7d36b05fd477dead53efc0d50b69906c83ac6b24
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-13 10:19:17 +0400

    create ticket model with its properties

---
+++ Models/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany_LINQ_EFCore.Models
+{
+    public class Ticket
+    {
+        public int TicketId { get; set; }
+        public string SeatNumber { get; set; }
+        public decimal Fare { get; set; }
+        public bool CheckedIn { get; set; }
+    }
+}
