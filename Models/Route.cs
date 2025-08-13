6f221538c057830f7770f435bbb2903dccc02cf0
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-13 10:00:22 +0400

    create Rote model with its properties

---
+++ Models/Route.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany_LINQ_EFCore.Models
+{
+    public class Route
+    {
+        public int RouteId { get; set; }
+        public int DistanceKm { get; set; } // Distance in kilometers
+        public string OriginCity { get; set; } // City of origin
+        public string DestinationCity { get; set; } // City of destination
+    }
+}
