89e005b6ee560dc4940390e7c51f73d6e57ec1de
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-13 08:15:28 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FlightManagementCompany_LINQ_EFCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
