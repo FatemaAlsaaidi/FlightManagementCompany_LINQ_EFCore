4f4cd06a23c34d12390fc6fec986c013d8d5bcf3
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-13 10:09:31 +0400

    create flight model with its propertioes

---
+++ Models/Flight.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany_LINQ_EFCore.Models
+{
+    public class Flight
+    {
+        public int FlightId { get; set; }
+        public string FlightNumber { get; set; } // Unique flight number  e.g., "FM101"
+        public DateTime DepartureUtc { get; set; }
+        public DateTime ArrivalUtc { get; set; } // UTC time of arrival
+        public string Status { get; set; } // e.g., "Scheduled", "Delayed", "Cancelled"
+    }
+}
