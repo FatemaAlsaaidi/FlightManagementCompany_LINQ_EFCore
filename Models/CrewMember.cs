8320f084fb9a26b3942a39eead4a398001869a60
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-13 09:56:40 +0400

    create CrewMember and its properties

---
+++ Models/CrewMember.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany_LINQ_EFCore.Models
+{
+    public class CrewMember
+    {
+        public int CrewId { get; set; }
+        public string Fname { get; set; }
+
+        public string Lname { get; set; }
+
+        public string Role { get; set; } // e.g., Pilot, Co-Pilot, Flight Attendant
+        public string LicenseNo { get; set; } // For pilots, the license number
+    }
+}
