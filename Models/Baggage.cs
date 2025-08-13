5a85d1f6161c964800604b5948c304cd9a76f76a
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-13 10:22:27 +0400

    create Baggage with ats properties

---
+++ Models/Baggage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany_LINQ_EFCore.Models
+{
+    public class Baggage
+    {
+        public int BaggageId { get; set; }
+        public decimal WeightKg { get; set; }
+        public string TagNumber { get; set; }
+    }
+}
