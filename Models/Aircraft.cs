bbd3a98596d4fc14c542b486bd54e19c61ebe0bd
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-13 09:53:48 +0400

    create Aircraft model with its properties

---
+++ Models/Aircraft.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany_LINQ_EFCore.Models
+{
+    public class Aircraft
+    {
+        public int AircraftId { get; set; }
+        public string TailNumber { get; set; }
+        public string Model { get; set; }
+        public int Capacity { get; set; }
+    }
+}
