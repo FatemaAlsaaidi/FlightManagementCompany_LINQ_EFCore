4c51acc16cc801c261b1eb3d401d7068ccbaef75
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-13 10:16:08 +0400

    create booking model with its properties

---
+++ Models/Booking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany_LINQ_EFCore.Models
+{
+    public class Booking
+    {
+        public int BookingId { get; set; }
+        public string BookingRef { get; set; }
+        public DateTime BookingDate { get; set; }
+        public string status { get; set; }
+    }
+}
