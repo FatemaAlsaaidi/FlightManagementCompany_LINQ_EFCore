a47981718a66c71e461efd68a920fcc81e60bf21
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-14 09:46:33 +0400

    create connect code to database

---
+++ FlightDatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlightManagementCompany_LINQ_EFCore
+{
+    public class FlightDatabaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-B1EOQP1 ;Initial Catalog=FlightManagementDB;Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
