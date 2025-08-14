18f7f0275e5b19fcbd8ffc3e3969e1399fbcae6c
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-14 15:55:26 +0400

    navigation and foriegn keys

--- Models/Flight.cs
+++ Models/Flight.cs
@@ -13,6 +13,20 @@ namespace FlightManagementCompany_LINQ_EFCore.Models
         public DateTime DepartureUtc { get; set; }
         public DateTime ArrivalUtc { get; set; } // UTC time of arrival
         public string Status { get; set; } // e.g., "Scheduled", "Delayed", "Cancelled"
+
+        public int RouteId { get; set; } // Foreign key to Route table
+        public int AircraftId { get; set; } // Foreign key to Aircraft table
+
         // coolection navigation to route 
+        public ICollection<Route> Routes { get; set; } = new List<Route>();
+
+        // collection navigation to aircraft
+        public Aircraft Aircraft { get; set; }
+
+        // navigation property to FlightCrew
+        public Route Route { get; set; } // Navigation property to Route
+
+        // navigation to Ticket
+        public ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
     }
 }
