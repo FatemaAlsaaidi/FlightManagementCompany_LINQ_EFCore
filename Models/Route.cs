98e4962cb1e9bc2162fb5ba97fc4cda224959db6
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-14 15:57:00 +0400

    navigation to origin airport + navigation to Destination airport + navigation to fligh

--- Models/Route.cs
+++ Models/Route.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FlightManagementCompany_LINQ_EFCore.Models
 {
@@ -10,7 +11,18 @@ namespace FlightManagementCompany_LINQ_EFCore.Models
     {
         public int RouteId { get; set; }
         public int DistanceKm { get; set; } // Distance in kilometers
-        public string OriginCity { get; set; } // City of origin
-        public string DestinationCity { get; set; } // City of destination
+        public string OriginOriginAirportId { get; set; } // Foriegn key to airport 
+        public string DestinationAirportId { get; set; } // Foriegn key to airport
+
+        // navigation to origin airport
+        [InverseProperty("OriginRoute")]
+        public Airport OriginAirport { get; set; } 
+        // navigation to Destination airport
+        [InverseProperty("DistenationRoute")]
+        public Airport DistenationAirport { get; set; } 
+        // navigation to flight
+        public ICollection<Flight> Flights { get; set; } = new List<Flight>();
+
+
     }
 }
