86a077d7443bedca72d63f06257b93d34b998d59
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-14 15:57:33 +0400

    Foreign key to Booking table + Foreign key to Flight table + navigation to booking  + navigation to flight

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -12,5 +12,17 @@ namespace FlightManagementCompany_LINQ_EFCore.Models
         public string SeatNumber { get; set; }
         public decimal Fare { get; set; }
         public bool CheckedIn { get; set; }
+
+        public int BookingId { get; set; } // Foreign key to Booking table
+        public int FlightId { get; set; } // Foreign key to Flight table
+
+        // navigation to booking 
+        public Booking Booking { get; set; }
+
+        // navigation to flight
+        public Flight Flight { get; set; }
+
+        // navigation to passengers
+        public ICollection<Passenger> Passengers { get; set; } = new List<Passenger>();
     }
 }
