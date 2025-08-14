fca6df6f4e7bf3c3d2c4e0c0c64b1bfba6e1d341
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-14 15:51:30 +0400

    navigation to Origin Route with Inverse + navigation to Destination Route with Inverse

--- Models/Airport.cs
+++ Models/Airport.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,5 +15,15 @@ namespace FlightManagementCompany_LINQ_EFCore.Models
         public string City { get; set; }
         public string Country { get; set; }
         public string TimeZone { get; set; }
+
+        [InverseProperty("OriginAirport")]
+        public ICollection<Route> OriginRoute { get; set; } = new List<Route>();// navigation to Origin Route
+
+        [InverseProperty("DistenationAirport")]
+        public ICollection<Route> DistenationRoute { get; set; } = new List<Route>();// navigation to Distenation Route
+
+
+
+
     }
 }
