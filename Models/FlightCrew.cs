4352e68a1214344e8a83e5d1c02678b35b05f109
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-14 15:56:15 +0400

    Role of the crew member on the flight (e.g., Pilot, Flight Attendant) + Unique identifier for the crew member + Foreign key to the Flight table + Navigation property to Flight + Navigation property to CrewMember

--- Models/FlightCrew.cs
+++ Models/FlightCrew.cs
@@ -8,6 +8,12 @@ namespace FlightManagementCompany_LINQ_EFCore.Models
 {
     public class FlightCrew
     {
-        public string RoleOnFlight;
+        public string RoleOnFlight { get; set; } // Role of the crew member on the flight (e.g., Pilot, Flight Attendant)
+        public int CrewId { get; set; } // Unique identifier for the crew member
+        public int FlightId { get; set; } // Foreign key to the Flight table
+
+        public Flight Flight { get; set; } // Navigation property to Flight
+
+        public CrewMember CrewMember { get; set; } // Navigation property to CrewMember
     }
 }
