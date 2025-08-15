5e5d4725dba60c528213e9cca4413122dbfbe4c8
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-15 15:04:40 +0400

    create FlightCrew repo which includes CRUD

---
+++ Repositories/FlightCrew.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany_LINQ_EFCore.Repositories
+{
+    public class FlightCrew
+    {
+        // database injection
+        private readonly FlightDatabaseContext _context;
+        public FlightCrew(FlightDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // Get all flight crews
+        public IEnumerable<Models.FlightCrew> GetAllFlightCrews()
+        {
+            return _context.FlightCrews;
+        }
+
+        // Get flight crew by ID
+        public Models.FlightCrew GetFlightCrewById(int CrewMemberId, int FlightId)
+        {
+            return _context.FlightCrews.FirstOrDefault(fc => fc.CrewId == CrewMemberId && fc.FlightId == FlightId);
+        }
+
+        // Add a new flight crew
+        public void AddFlightCrew(Models.FlightCrew flightCrew)
+        {
+            
+            _context.FlightCrews.Add(flightCrew);
+            _context.SaveChanges();
+        }
+
+        // Update an existing flight crew
+        public void UpdateFlightCrew(Models.FlightCrew flightCrew)
+        {
+         
+            _context.FlightCrews.Update(flightCrew);
+            _context.SaveChanges();
+        }
+
+        // Delete a flight crew
+        public void DeleteFlightCrew(Models.FlightCrew flightCrew)
+        {
+
+            _context.FlightCrews.Remove(flightCrew);
+            _context.SaveChanges();
+        }
+
+    }
+}
