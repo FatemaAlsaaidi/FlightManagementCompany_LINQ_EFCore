414bb506deb0a4bd26b75c097f026eb8473439d2
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-15 15:09:14 +0400

    Create Route repo  witch includes CRUD

---
+++ Repositories/RouteRepo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlightManagementCompany_LINQ_EFCore.Models;
+
+namespace FlightManagementCompany_LINQ_EFCore.Repositories
+{
+    public class RouteRepo
+    {
+        // Database injection
+        private readonly FlightDatabaseContext _context;
+        public RouteRepo(FlightDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // Get All Routes
+        public List<Route> GetAllRoutes()
+        {
+            return _context.Routes.ToList();
+        }
+
+        // Get Route by ID
+        public Route GetRouteById(int id)
+        {
+            return _context.Routes.FirstOrDefault(r => r.RouteId == id);
+        }
+
+        // Add a new Route
+        public void AddRoute(Route route)
+        {
+            _context.Routes.Add(route);
+            _context.SaveChanges();
+        }
+        // Update an existing Route
+        public void UpdateRoute(Route route)
+        {
+            _context.Routes.Update(route);
+            _context.SaveChanges();
+        }
+        // Delete a Route
+        public void DeleteRoute(Route route)
+        {
+            _context.Routes.Remove(route);
+            _context.SaveChanges();
+        }
+    }
+}
