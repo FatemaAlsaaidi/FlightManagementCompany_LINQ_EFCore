3850d58f76789bf42d340812b5366984420c2199
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-15 15:06:36 +0400

    CREATE Flight repo which includes CRUD

---
+++ Repositories/FlightRepo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlightManagementCompany_LINQ_EFCore.Models;
+
+namespace FlightManagementCompany_LINQ_EFCore.Repositories
+{
+    public class FlightRepo
+    {
+        // Database injection
+
+        private readonly FlightDatabaseContext _context;
+        public FlightRepo(FlightDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // Get All Flights
+
+        public List<Flight> GetAllFlights()
+        {
+            return _context.Flights.ToList();
+        }
+
+        // Get Flight by ID
+        public Flight GetFlightById(int id)
+        {
+            return _context.Flights.FirstOrDefault(f => f.FlightId == id);
+        }
+        // Add a new Flight
+        public void AddFlight(Flight flight)
+        {
+            _context.Flights.Add(flight);
+            _context.SaveChanges();
+        }
+        // Update an existing Flight
+        public void UpdateFlight(Flight flight)
+        {
+            _context.Flights.Update(flight);
+            _context.SaveChanges();
+        }
+
+        // Delete a Flight
+        public void DeleteFlight(Flight flight)
+        {
+           
+                _context.Flights.Remove(flight);
+                _context.SaveChanges();
+            
+        }
+
+    }
+}
