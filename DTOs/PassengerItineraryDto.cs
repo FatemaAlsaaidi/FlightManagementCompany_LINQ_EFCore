6bba25948664a0073e80660c0d229bd8f9593fb9
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-16 07:12:15 +0400

    create PassengerItineraryDto + ItinSegmentDto

---
+++ DTOs/PassengerItineraryDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany_LINQ_EFCore.DTOs
+{
+    public class PassengerItineraryDto
+    {
+
+        public int PassengerId { get; set; }
+        public string PassengerName { get; set; } = string.Empty;
+        public List<ItinSegmentDto> Segments { get; set; } = new();
+        
+
+       
+    }
+
+    public class ItinSegmentDto
+    {
+        public int FlightId { get; set; }
+        public string FlightNumber { get; set; } = string.Empty;
+        public string OriginIata { get; set; } = string.Empty;
+        public string DestinationIata { get; set; } = string.Empty;
+        public DateTime DepUtc { get; set; }
+        public DateTime ArrUtc { get; set; }
+    }
+}
