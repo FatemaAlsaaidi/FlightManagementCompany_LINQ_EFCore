d512c3c1c5246f2168ce1d2db3ec27d0de4bcfb9
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-16 07:08:01 +0400

    create class FlightManifestDto

---
+++ DTOs/FlightManifestDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany_LINQ_EFCore.DTOs
+{
+    public class FlightManifestDto
+    {
+        public string FlightNumber { get; set; } = string.Empty;
+        public string Origin { get; set; } = string.Empty;      // IATA of origin
+        public string Destination { get; set; } = string.Empty;      // IATA of destination
+        public DateTime DepUtc { get; set; }
+        public DateTime ArrUtc { get; set; }
+        public string AircraftTail { get; set; } = string.Empty;
+        public int PassengerCount { get; set; }
+        public decimal TotalBaggageKg { get; set; }
+        public List<CrewDto> Crew { get; set; } = new();
+
+        public class CrewDto
+        {
+            public string Name { get; set; } = string.Empty; // e.g., "Ali Hamed"
+            public string Role { get; set; } = string.Empty; // e.g., "Pilot", "CoPilot", "FlightAttendant"
+        }
+    }
+}
