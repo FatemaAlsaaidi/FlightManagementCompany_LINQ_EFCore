670b383dc0ef378dec445599a9dc94f391d7563c
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-17 08:17:35 +0400

    MaintenanceAlertDto

---
+++ DTOs/MaintenanceAlertDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany_LINQ_EFCore.DTOs
+{
+    public class MaintenanceAlertDto
+    {
+        public int AircraftId { get; set; }
+        public string TailNumber { get; set; } = string.Empty;
+        public double TotalHours { get; set; }
+        public int DaysSinceLastMaintenance { get; set; }
+        public string Reason { get; set; } = string.Empty;
+    }
+}
