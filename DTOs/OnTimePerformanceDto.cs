2a09f59ad2b76a3a79a60daada012481137b6a9d
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-17 08:18:03 +0400

    OnTimePerformanceDto

---
+++ DTOs/OnTimePerformanceDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany_LINQ_EFCore.DTOs
+{
+    public class OnTimePerformanceDto
+    {
+        // Grouping key, e.g. "MCT->DXB" (per route) or any label you choose
+        public string Key { get; set; } = string.Empty;
+
+        // How many flights in the group
+        public int Flights { get; set; }
+
+        // How many were on time (within threshold minutes)
+        public int OnTime { get; set; }
+
+        // Convenience computed property
+        public double OnTimePercent => Flights == 0 ? 0 : (OnTime * 100.0) / Flights;
+    }
+}
