64dcf78b3e1ca8aee07bed5aabb28de615135940
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-17 08:18:19 +0400

    SimpleRouteDto

---
+++ DTOs/SimpleRouteDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany_LINQ_EFCore.DTOs
+{
+    public class SimpleRouteDto
+    {
+        public int RouteId { get; set; }
+        public string Origin { get; set; } = string.Empty;      // IATA
+        public string Destination { get; set; } = string.Empty;      // IATA
+    }
+}
