1bb230a8772e196ac3a9fa5d1af0020d21eab9f5
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-17 08:17:03 +0400

    BaggageOverweightDto

---
+++ DTOs/BaggageOverweightDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany_LINQ_EFCore.DTOs
+{
+    public class BaggageOverweightDto
+    {
+        public int TicketId { get; set; }
+        public string FlightNumber { get; set; } = string.Empty;
+        public string PassengerName { get; set; } = string.Empty;
+        public decimal TotalBaggageKg { get; set; }
+    }
+}
