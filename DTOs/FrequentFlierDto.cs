7c8f1c07a65d198bf7035870ea2fc51d519e2905
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-17 08:17:16 +0400

    FrequentFlierDto

---
+++ DTOs/FrequentFlierDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementCompany_LINQ_EFCore.DTOs
+{
+    public class FrequentFlierDto
+    {
+        public int PassengerId { get; set; }
+        public string PassengerName { get; set; } = string.Empty;
+        public int FlightsCount { get; set; }
+        public int TotalDistanceKm { get; set; }   // when byDistance = true
+    }
+}
