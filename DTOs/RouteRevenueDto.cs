24034aaa0c3114146d97222c3afebb18dc067a1f
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-23 11:31:57 +0400

    add new attributes and rename exist attribues

--- DTOs/RouteRevenueDto.cs
+++ DTOs/RouteRevenueDto.cs
@@ -9,10 +9,12 @@ namespace FlightManagementCompany_LINQ_EFCore.DTOs
     public class RouteRevenueDto
     {
         public int RouteId { get; set; }
-        public string Origin { get; set; } = string.Empty;      // Origin IATA
-        public string Destination { get; set; } = string.Empty;      // Destination IATA
+        public string OriginIATA { get; set; } = string.Empty;      // Origin IATA
+        public string DestinationIATA { get; set; } = string.Empty;      // Destination IATA
         public decimal Revenue { get; set; }                      // Sum of fares
+        public decimal DistanceKm { get; set; }                     // Distance in kilometers
         public int SeatsSold { get; set; }                      // Count of tickets
+        public string RouteCountries { get; set; } = string.Empty; // Comma-separated list of countries for the route
         public decimal AvgFare { get; set; }                      // Revenue / SeatsSold
     }
 }
