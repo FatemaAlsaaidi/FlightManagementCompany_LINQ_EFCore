2c057a0264a0bf3812f257967bb89dd21202f05f
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-23 11:30:23 +0400

    new migration to add new attribute to flight table in database

---
+++ Migrations/20250823070541_ADDNewAttributeToFlightModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlightManagementCompany_LINQ_EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class ADDNewAttributeToFlightModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ActualArrivalUtc",
+                table: "Flights",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ActualArrivalUtc",
+                table: "Flights");
+        }
+    }
+}
